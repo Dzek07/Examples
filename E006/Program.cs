d3632987e86029117927dd109de60c9d14ea09e3
Dzek <dzaek07@gmail.com>
2022-08-16 22:08:43 +0300

    E006

---
+++ E006/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите ваше имя ");
+string usernane = Console.ReadLine();
+if(usernane.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(usernane);
+}
+
